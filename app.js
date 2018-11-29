3923e12f9fe881e78e8a9176832f3acc24b2dd5d
quahjieren@gmail.com <quahjieren@gmail.com>
2018-11-29 17:23:20 +0800

    Load express, request, cors, querystring, cookieParser modules in app.js.

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-var client_id = '930da2356b374ffca2da7903affee68f';  // insert unique user token here
-var client_secret = '';  //  possibly like a 'password'. maybe need to get info from Spotify Developer account. DO NOT PUSH THIS TO GIT.
-var redirect_uri = 'localhost:8888';  // figure out what this is, not sure yet
+// Load Express web server framework module
+var express = require('express');
 
-var scopes = /'user-read-private user-read-email'/  // specify what the app should request for. in this case full name, profile image and email address.
+// Load request module
+var request = require('request');
+
+/* Load Cross Origin Resource Sharing (CORS) module. 
+Further info: https://flaviocopes.com/cors/ */
+var cors = require('cors');
+
+/* Load querystring module. Allows parsing and stringifying of URL query strings.
+Further info: https://www.npmjs.com/package/query-string */
+var querystring = require('querystring');
+
+/* Load cookieParser module. Parses cookie header and handles cookie separation and encoding. Decrypts signed cookies if you know the secret.
+Further info: https://www.npmjs.com/package/cookie-parser */
+var cookieParser = require('cookie-parser');
+
+ // insert unique user token
+var client_id = '930da2356b374ffca2da7903affee68f';
+
+//  possibly like a 'password'. maybe need to get info from Spotify Developer account. DO NOT PUSH THIS TO GIT.
+var client_secret = '';
+
+// set redirect address after user authentication
+var redirect_uri = 'localhost:8888';
+
+// specify what the app should request for. in this case full name, profile image and email address.
+var scopes = /'user-read-private user-read-email'/;
 
 /* 
 There should be a total of three calls to the Spotify Accounts Service.
